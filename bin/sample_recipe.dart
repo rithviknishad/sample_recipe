9760fbc76f4b124fc9805fb2efea89e149cf3ad1
Rithvik Nishad <rithvikn2001@gmail.com>
2021-08-10 17:34:55 +0530

    impl. sample recipe

--- bin/sample_recipe.dart
+++ bin/sample_recipe.dart
@@ -1,4 +1,10 @@
-void main(List<String> arguments) {
-  print('Hello world!');
-  final Function a;
+import 'package:recipe/recipe.dart';
+
+void main(List<String> arguments) => bake(SampleRecipe());
+
+class SampleRecipe extends Recipe {
+  @override
+  RecipeStream bake(BakeContext context) async* {
+    print('$name#$hashCode is being baked');
+  }
 }
